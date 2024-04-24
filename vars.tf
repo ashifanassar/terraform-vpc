a2f2622248cbbd5dc2bcdcbc375d90c3deaa9eb2
ashifanassar <ashifanassar@gmail.com>
2024-04-24 16:47:38 +0530

    terraform modules

--- vars.tf
+++ vars.tf
@@ -1,7 +1,15 @@
-variable "VPC_CIDR" {}
+variable "VPC_CIDR" {
+    default     = "VPC_CIDR"
+}
 
-variable "PUBLIC_SUBNET_CIDR" {}
+variable "PUBLIC_SUBNET_CIDR" {
+    default     = ["PUBLIC_SUBNET_CIDR"]
+}
 
-variable "PRIVATE_SUBNET_CIDR" {}
+variable "PRIVATE_SUBNET_CIDR" {
+    default     = ["PRIVATE_SUBNET_CIDR"]
+}
 
-variable "ENV" {}
+variable "ENV" {
+    default     = "ExampleAppServerInstance"
+}
