30ebab53cef8153835160ae4572b11fa00869d46
ashifanassar <ashifanassar@gmail.com>
2024-04-24 23:21:54 +0530

    terraform modules

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 module "vpc" {
-  source      = "./vendor/modules/vpc/"
-  VPC_CIDR    =  var.VPC_CIDR
-  PUBLIC_SUBNET_CIDR = var.PUBLIC_SUBNET_CIDR
-  PRIVATE_SUBNET_CIDR = var.PRIVATE_SUBNET_CIDR
+    source      = "./vendor/modules/vpc/"
+    VPC_CIDR    =  var.VPC_CIDR
+    PUBLIC_SUBNET_CIDR = var.PUBLIC_SUBNET_CIDR
+    PRIVATE_SUBNET_CIDR = var.PRIVATE_SUBNET_CIDR
 }
